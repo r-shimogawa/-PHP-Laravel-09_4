fd9fbd5fd8a113131469fee015c1f4ad22febd51
r-shimogawa <shimogawa1992@gmail.com>
2022-11-06 14:09:12 +0000

    first commmit

--- ProfileController.php
+++ ProfileController.php
@@ -5,23 +5,43 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// 【PHP/Laravel】09　4
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
     {
         return view('admin.profile.create');
     }
-        public function create()
+        public function create(Request $request)
     {
+    // 以下を追記
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();    
+        
+        
         return redirect('admin/profile/create');
     }
         public function edit()
     {
-        return view('admin.profile.create');
+        return view('admin.profile.edit');
     }
         public function update()
     {
-        return redirect('admin/profile/create');
+        return redirect('admin/profile/edit');
     }
         
 }
